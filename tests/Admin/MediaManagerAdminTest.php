82d668591827dd8820c2fda0947c570fa6c6eab3
Andrew Lowther <andrew.lowther@mademedia.co.uk>
2013-08-02 09:19:26 +0100

    Adding tests base, not currently implementing anything
    Signed-off-by: Andrew Lowther <andrew.lowther@mademedia.co.uk>

---
+++ tests/Admin/MediaManagerAdminTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Media manager admin tests
+ * 
+ * @author 		Andrew Lowther
+ * @package 	Media Manager
+ * @version 	Release: 0.1.0
+ * @copyright 	2013 Andrew Lowther
+ * @link 		https://github.com/AndrewLowther/mediamanager
+ * @since 		0.1.0
+ */
+class MediaManagerAdminTest extends SapphireTest {
+
+	/**
+	 * Fixture file, represents the database
+	 * 
+	 * @var string
+	 * @access public
+	 */
+	public static $fixture_file = 'mediamanager/tests/MediaManagerAdminTest.yml';
+
+}
