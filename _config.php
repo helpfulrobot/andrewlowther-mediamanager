6cd06c4b12978a524a459a61267318b6dfcd62e5
Andrew Lowther <andrew.lowther@mademedia.co.uk>
2013-08-05 12:23:07 +0100

    Fixing a variable by reference failure
    Signed-off-by: Andrew Lowther <andrew.lowther@mademedia.co.uk>

--- _config.php
+++ _config.php
@@ -1,7 +1,10 @@
 <?php
 
+$ds = DIRECTORY_SEPARATOR;
 if (version_compare(PHP_VERSION, '5.3.0') >= 0) {
-	define('MEDIAMANAGER_CORE_PATH', end(explode(DIRECTORY_SEPARATOR, __DIR__)));
+	$path = __DIR__;
 } else {
-	define('MEDIAMANAGER_CORE_PATH', end(explode(DIRECTORY_SEPARATOR, dirname(__FILE__))));
+	$path = dirname(__FILE__);
 }
+$mediamanager_core_path = explode($ds, $path);
+define('MEDIAMANAGER_CORE_PATH', end($mediamanager_core_path));
