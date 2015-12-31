f5068decffac3534737f99b34248d8a85a25033b
helpfulrobot <chris@silverstripe.com>
2016-01-01 03:50:04 +1300

    Converted to PSR-2

--- tests/Admin/MediaManagerAdminTest.php
+++ tests/Admin/MediaManagerAdminTest.php
@@ -10,87 +10,92 @@
  * @link 		https://github.com/AndrewLowther/mediamanager
  * @since 		0.1.0
  */
-class MediaManagerAdminTest extends SapphireTest {
+class MediaManagerAdminTest extends SapphireTest
+{
 
-	/**
-	 * Fixture file, represents the database
-	 * 
-	 * @var string
-	 * @access public
-	 */
-	public static $fixture_file = 'mediamanager/tests/Admin/MediaManagerAdminTest.yml';
+    /**
+     * Fixture file, represents the database
+     * 
+     * @var string
+     * @access public
+     */
+    public static $fixture_file = 'mediamanager/tests/Admin/MediaManagerAdminTest.yml';
 
-	/**
-	 * Original environment type
-	 *
-	 * @var string
-	 * @access protected
-	 **/
-	protected $orig_env_type;
+    /**
+     * Original environment type
+     *
+     * @var string
+     * @access protected
+     **/
+    protected $orig_env_type;
 
-	/**
-	 * Set up for the series of tests
-	 *
-	 * @return void
-	 * @author Andrew Lowther <andrew.lowther@mademedia.co.uk>
-	 **/
-	public function setUpOnce() {
-		$this->orig_env_type = Config::inst()->get('Director', 'environment_type');
-		Config::inst()->update('Director', 'environment_type', 'dev');
+    /**
+     * Set up for the series of tests
+     *
+     * @return void
+     * @author Andrew Lowther <andrew.lowther@mademedia.co.uk>
+     **/
+    public function setUpOnce()
+    {
+        $this->orig_env_type = Config::inst()->get('Director', 'environment_type');
+        Config::inst()->update('Director', 'environment_type', 'dev');
 
-		parent::setUpOnce();
-	}
+        parent::setUpOnce();
+    }
 
-	/**
-	 * Tear down the test when we're done
-	 * 
-	 * @return void
-	 * @author Andrew Lowther <andrew.lowther@mademedia.co.uk>
-	 */
-	public function tearDownOnce() {
-		Config::inst()->update('Director', 'environment_type', $this->orig_env_type);
+    /**
+     * Tear down the test when we're done
+     * 
+     * @return void
+     * @author Andrew Lowther <andrew.lowther@mademedia.co.uk>
+     */
+    public function tearDownOnce()
+    {
+        Config::inst()->update('Director', 'environment_type', $this->orig_env_type);
 
-		parent::tearDownOnce();
-	}
+        parent::tearDownOnce();
+    }
 
-	/**
-	 * Return a session that has a user logged in as an administrator
-	 * 
-	 * @return Session
-	 */
-	public function adminLoggedInSession() {
-		return new Session(array(
-			'loggedInAs' => $this->idFromFixture('Member', 'admin')
-		));
-	}
+    /**
+     * Return a session that has a user logged in as an administrator
+     * 
+     * @return Session
+     */
+    public function adminLoggedInSession()
+    {
+        return new Session(array(
+            'loggedInAs' => $this->idFromFixture('Member', 'admin')
+        ));
+    }
 
-	/**
-	 * Test the primary list view
-	 * 
-	 * @return void
-	 * @author Andrew Lowther <andrew.lowther@mademedia.co.uk>
-	 */
-	public function testListView() {
-		// Open the admin area logged in as admin
-		$response = Director::test('admin/media/', null, $this->adminLoggedInSession());
+    /**
+     * Test the primary list view
+     * 
+     * @return void
+     * @author Andrew Lowther <andrew.lowther@mademedia.co.uk>
+     */
+    public function testListView()
+    {
+        // Open the admin area logged in as admin
+        $response = Director::test('admin/media/', null, $this->adminLoggedInSession());
 
-		// Confirm that this URL gets you the entire page, with the edit form loaded
-		$this->assertTrue(strpos($response->getBody(), 'Add Media Item') !== false);
-		$this->assertTrue(strpos($response->getBody(), 'ss-gridfield-table') !== false);
-		$this->assertTrue(strpos($response->getBody(), 'window.mediamanager.cloudinary') !== false);
-	}
+        // Confirm that this URL gets you the entire page, with the edit form loaded
+        $this->assertTrue(strpos($response->getBody(), 'Add Media Item') !== false);
+        $this->assertTrue(strpos($response->getBody(), 'ss-gridfield-table') !== false);
+        $this->assertTrue(strpos($response->getBody(), 'window.mediamanager.cloudinary') !== false);
+    }
 
-	/**
-	 * Test the add media view
-	 *
-	 * @return void
-	 * @author Andrew Lowther <andrew.lowther@mademedia.co.uk>
-	 **/
-	public function testAddItemView() {
-		$response = Director::test('admin/media/MediaItem/EditForm/field/MediaItem/item/new', null, $this->adminLoggedInSession());
+    /**
+     * Test the add media view
+     *
+     * @return void
+     * @author Andrew Lowther <andrew.lowther@mademedia.co.uk>
+     **/
+    public function testAddItemView()
+    {
+        $response = Director::test('admin/media/MediaItem/EditForm/field/MediaItem/item/new', null, $this->adminLoggedInSession());
 
-		$this->assertTrue(strpos($response->getBody(), 'name="CloudinaryRef"') !== false);
-		$this->assertTrue(strpos($response->getBody(), 'Create') !== false);
-	}
-
+        $this->assertTrue(strpos($response->getBody(), 'name="CloudinaryRef"') !== false);
+        $this->assertTrue(strpos($response->getBody(), 'Create') !== false);
+    }
 }
--- tests/Cloudinary/CloudinarySimpleImageTest.php
+++ tests/Cloudinary/CloudinarySimpleImageTest.php
@@ -10,108 +10,115 @@
  * @link 		https://github.com/AndrewLowther/mediamanager
  * @since 		0.1.0
  */
-class CloudinarySimpleImageTest extends SapphireTest {
+class CloudinarySimpleImageTest extends SapphireTest
+{
 
-	/**
-	 * Original environment type
-	 *
-	 * @var string
-	 * @access protected
-	 **/
-	protected $orig_env_type;
+    /**
+     * Original environment type
+     *
+     * @var string
+     * @access protected
+     **/
+    protected $orig_env_type;
 
-	/**
-	 * Config object
-	 *
-	 * @var Config
-	 **/
-	protected $config;
+    /**
+     * Config object
+     *
+     * @var Config
+     **/
+    protected $config;
 
-	/**
-	 * Constructor
-	 *
-	 * @return void
-	 * @author Andrew Lowther <andrew.lowther@mademedia.co.uk>
-	 **/
-	public function __construct() {
-		$this->config = Config::inst()->get('MediaManager', 'Cloudinary');
-		\Cloudinary::config($this->config);
-	}
+    /**
+     * Constructor
+     *
+     * @return void
+     * @author Andrew Lowther <andrew.lowther@mademedia.co.uk>
+     **/
+    public function __construct()
+    {
+        $this->config = Config::inst()->get('MediaManager', 'Cloudinary');
+        \Cloudinary::config($this->config);
+    }
 
-	/**
-	 * Set up for the series of tests
-	 *
-	 * @return void
-	 * @author Andrew Lowther <andrew.lowther@mademedia.co.uk>
-	 **/
-	public function setUpOnce() {
-		$this->orig_env_type = Config::inst()->get('Director', 'environment_type');
-		Config::inst()->update('Director', 'environment_type', 'dev');
+    /**
+     * Set up for the series of tests
+     *
+     * @return void
+     * @author Andrew Lowther <andrew.lowther@mademedia.co.uk>
+     **/
+    public function setUpOnce()
+    {
+        $this->orig_env_type = Config::inst()->get('Director', 'environment_type');
+        Config::inst()->update('Director', 'environment_type', 'dev');
 
-		parent::setUpOnce();
-	}
+        parent::setUpOnce();
+    }
 
-	/**
-	 * Tear down the test when we're done
-	 * 
-	 * @return void
-	 * @author Andrew Lowther <andrew.lowther@mademedia.co.uk>
-	 */
-	public function tearDownOnce() {
-		Config::inst()->update('Director', 'environment_type', $this->orig_env_type);
+    /**
+     * Tear down the test when we're done
+     * 
+     * @return void
+     * @author Andrew Lowther <andrew.lowther@mademedia.co.uk>
+     */
+    public function tearDownOnce()
+    {
+        Config::inst()->update('Director', 'environment_type', $this->orig_env_type);
 
-		parent::tearDownOnce();
-	}
+        parent::tearDownOnce();
+    }
 
-	/**
-	 * Test the cloudinary config
-	 *
-	 * @return void
-	 * @author Andrew Lowther <andrew.lowther@mademedia.co.uk>
-	 **/
-	public function testCloudinaryConfig() {
-		$this->assertTrue(\Cloudinary::config_get('cloud_name') === $this->config['cloud_name']);
-		$this->assertTrue(\Cloudinary::config_get('api_key') === $this->config['api_key']);
-		$this->assertTrue(\Cloudinary::config_get('api_secret') === $this->config['api_secret']);
-	}
+    /**
+     * Test the cloudinary config
+     *
+     * @return void
+     * @author Andrew Lowther <andrew.lowther@mademedia.co.uk>
+     **/
+    public function testCloudinaryConfig()
+    {
+        $this->assertTrue(\Cloudinary::config_get('cloud_name') === $this->config['cloud_name']);
+        $this->assertTrue(\Cloudinary::config_get('api_key') === $this->config['api_key']);
+        $this->assertTrue(\Cloudinary::config_get('api_secret') === $this->config['api_secret']);
+    }
 
-	/**
-	 * Test image upload to cloudinary
-	 *
-	 * @return void
-	 * @author Andrew Lowther <andrew.lowther@mademedia.co.uk>
-	 **/
-	public function testCloudinaryImageUpload() {
-		$response = \Cloudinary\Uploader::upload('mediamanager/assets/test/test.png', array(
-			'public_id' => 'test.png'
-		));
-		$this->assertTrue(array_key_exists('public_id', $response));
-	}
+    /**
+     * Test image upload to cloudinary
+     *
+     * @return void
+     * @author Andrew Lowther <andrew.lowther@mademedia.co.uk>
+     **/
+    public function testCloudinaryImageUpload()
+    {
+        $response = \Cloudinary\Uploader::upload('mediamanager/assets/test/test.png', array(
+            'public_id' => 'test.png'
+        ));
+        $this->assertTrue(array_key_exists('public_id', $response));
+    }
 
-	/**
-	 * Test the image fetch
-	 *
-	 * @return void
-	 * @author Andrew Lowther <andrew.lowther@mademedia.co.uk>
-	 **/
-	public function testCloudinaryImageFetch() {
-		$mediaItem = DataObject::get_by_id('MediaItem', 1);
-		$image = cloudinary_url('test.png');
+    /**
+     * Test the image fetch
+     *
+     * @return void
+     * @author Andrew Lowther <andrew.lowther@mademedia.co.uk>
+     **/
+    public function testCloudinaryImageFetch()
+    {
+        $mediaItem = DataObject::get_by_id('MediaItem', 1);
+        $image = cloudinary_url('test.png');
 
-		$this->assertTrue($image === sprintf('http://res.cloudinary.com/%s/image/upload/test.png', $this->config['cloud_name']));
-	}
+        $this->assertTrue($image === sprintf('http://res.cloudinary.com/%s/image/upload/test.png', $this->config['cloud_name']));
+    }
 
-	/**
-	 * Test image deletion
-	 *
-	 * @return void
-	 * @author Andrew Lowther <andrew.lowther@mademedia.co.uk>
-	 **/
-	public function testCloudinaryImageDeletion() {
-		$api = new \Cloudinary\Api();
-		$result = $api->delete_resources(array('test.png'));
-		
-		$this->assertTrue(is_a($result, 'Cloudinary\Api\Response'));
-	}
-
+    /**
+     * Test image deletion
+     *
+     * @return void
+     * @author Andrew Lowther <andrew.lowther@mademedia.co.uk>
+     **/
+    public function testCloudinaryImageDeletion()
+    {
+        $api = new \Cloudinary\Api();
+        $result = $api->delete_resources(array('test.png'));
+        
+        $this->assertTrue(is_a($result, 'Cloudinary\Api\Response'));
+    }
 }
